f2c2e7376ea1cfa2d1938cbc35cce001fa971c80
samsamyyy12 <samara.kelly@urfpe.br>
2023-10-15 14:39:09 -0300

    aula1

---
+++ src/main/java/rocketseat/todolist/user/userController.java
@@ -0,0 +1,15 @@
+package rocketseat.todolist.user;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class userController {
+    
+    @PostMapping("/")
+    public void create(@RequestBody userModel userModel){
+        System.out.println(userModel.name);
+    }
+}
---
+++ src/main/java/rocketseat/todolist/user/userModel.java
@@ -0,0 +1,7 @@
+package rocketseat.todolist.user;
+
+public class userModel{
+    public String username;
+    public String name;
+    public String password;
+}
