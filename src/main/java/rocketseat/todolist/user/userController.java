cae0cdbc9d7b12df2591205de9345cb112062afb
samsamyyy12 <samara.kelly@urfpe.br>
2023-10-15 15:30:57 -0300

    aula2

--- src/main/java/rocketseat/todolist/user/userController.java
+++ src/main/java/rocketseat/todolist/user/userController.java
@@ -1,4 +1,7 @@
 package rocketseat.todolist.user;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class userController {
     
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody userModel userModel){
-        System.out.println(userModel.getName());
+    public ResponseEntity create(@RequestBody userModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user!=null){
+            return ResponseEntity.status(400).body("deu ruim");
+        }
+
+        var userCreated =this.userRepository.save(userModel);
+        return ResponseEntity.status(201).body("jóia");
     }
 }
